ef3844470e9d9e1fd0aa73adad9a34e58b6fc718
shotas7788 <shotas7788@vivaldi.net>
2019-12-29 09:38:37 +0900

    docker

--- Vagrantfile
+++ Vagrantfile
@@ -32,7 +32,7 @@ Vagrant.configure("2") do |config|
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
-  # config.vm.network "private_network", ip: "192.168.33.10"
+   config.vm.network "private_network", ip: "192.168.33.10"
 
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
@@ -67,4 +67,7 @@ Vagrant.configure("2") do |config|
   #   apt-get update
   #   apt-get install -y apache2
   # SHELL
+  
+  # [初回のみ]必要なパッケージのインストール等初期設定
+  config.vm.provision :shell, :path => "provision/provision.sh"
 end
---
+++ provision/provision.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+echo -------------------------------
+echo
+echo historyフォーマット設定
+echo
+echo -------------------------------
+
+HISTTIMEFORMAT='%y/%m/%d %H:%M:%S ';
+
+echo -------------------------------
+echo
+echo タイムゾーン設定
+echo
+echo -------------------------------
+
+sudo cp /usr/share/zoneinfo/Japan /etc/localtime
+date
+
+# インストール済みのパッケージをアップデート
+sudo yum -y update
+
+# よく使うコマンドをインストール
+sudo yum -y install wget unzip yum-utils mlocate
+sudo updatedb
+
+# epel, remi リポジトリを追加して有効化
+sudo yum -y install epel-release
+sudo rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sudo yum-config-manager --enable epel remi
+sudo yum repolist all
+
+# dockerインストール
+# 古いバージョンのDockerをアンインストール
+sudo yum remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-engine
+
+# 必要なパッケージをインストール
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+# 安定版が含まれるリポジトリを設定
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+# 最新バージョンのDocker CEをインストール
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+# Docker を起動して、hello-world イメージを実行
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world
+
+# dockerバージョン確認
+docker --version
+
+# 現行ユーザをdockerグループに所属させる
+# （dockerがインストールされた時点でdockerグループは作られる）
+sudo gpasswd -a $USER docker
+
+# dockerデーモンを再起動する (CentOS7の場合)
+sudo systemctl restart docker
